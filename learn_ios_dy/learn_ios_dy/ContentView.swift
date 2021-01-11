3110817a5c33edefb0915b132fb91af99bbfe5e1
鑫 林 <linxin@mampod.com>
2021-01-11 16:03:02 +0800

    初始化项目

---
+++ learn_ios_dy/learn_ios_dy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn_ios_dy
+//
+//  Created by QA林鑫 on 2021/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learn_ios_dy/learn_ios_dy/learn_ios_dyApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_ios_dyApp.swift
+//  learn_ios_dy
+//
+//  Created by QA林鑫 on 2021/1/11.
+//
+
+import SwiftUI
+
+@main
+struct learn_ios_dyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
